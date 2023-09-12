d86cd4ba08ff65ae3082cfbe5ad5b2c1017ad508
salyga_yaroslav <salyga.yaroslav@gmail.com>
2023-09-12 17:00:20 +0300

    1 lesson

---
+++ request.js
@@ -0,0 +1,29 @@
+const axios = require("axios");
+const fs = require("fs");
+const http = require("http");
+
+function func() {
+  return new Promise((resolve, reject) => {
+    console.log("request start");
+    axios
+      .get("https://jsonplaceholder.typicode.com/todos/1")
+      .then((response) => {
+        const data = JSON.stringify(response.data);
+        fs.writeFileSync("request.json", data);
+
+        resolve(data);
+      })
+      .catch((error) => reject(error));
+  });
+}
+
+const server = http.createServer((request, response) => {
+  func().then((data) => {
+    response.writeHead(200, { "Content-Type": "text/json" });
+    response.end(data);
+  });
+});
+
+server.listen(3000, () => {
+  console.log("Server running at http://127.0.0.1:3000/");
+});
